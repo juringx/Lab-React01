303a683f064b4ca25f9ef015f0bec4a403d24042
juringx <6310110074@psu.ac.th>
2022-08-20 23:04:45 +0700

    part5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
+
+const word = "Hello";
 function App() {
   return (
     <div>
-      <CharacterCard value="h"/>
-      <CharacterCard value="i"/>
+      <WordCard value={word}/>
     </div>
   );
 }
+
 export default App;
